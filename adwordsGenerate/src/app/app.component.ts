f81c7e46986f45ed377411e8ff286093523737dd
Seospan <sophie.jobez@gmail.com>
2017-05-29 21:46:28 +0200

    Stable

--- adwordsGenerate/src/app/app.component.ts
+++ adwordsGenerate/src/app/app.component.ts
@@ -8,13 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'app works!';
 
-  nbMots : number = 5;
+  nbMots : number = 2;
 
   mots;
   indexes;
 
   splittedMots = [];
   generatedWordJoinsList = [];
+
+  addField(){
+    this.nbMots++;
+    this.generateFormList();
+  }
+
+  removeField(){
+    this.nbMots--;
+    this.generateFormList();
+  }
 
   generateList(){
      console.log("Liste de mots");
@@ -25,49 +35,61 @@ export class AppComponent implements OnInit {
      console.log("List splittée");
       console.log(this.splittedMots);
 
-      this.generatedWordJoinsList = [];
-      this.generateWordJoins(this.splittedMots,this.generatedWordJoinsList);
+      this.generatedWordJoinsList = this.generateWordJoins(this.splittedMots);
+      console.log("Result");
+      console.log(this.generatedWordJoinsList)
   }
 
-  generateWordJoins(splittedMots,listToFill){
+  generateWordJoins(splittedMots){
+    let listToFill = [];
     if(splittedMots.length>0){
-        //let splittedMotsTemp = splittedMots;
-        //let listToProcess = splittedMotsTemp.splice(0,1);
-        //let listToProcess = splittedMots.splice(0,1);
-        //console.log("new list");
-        //console.log(splittedMotsTemp);
-        for(let i in splittedMots){
-          let listToProcess = splittedMots[i];
-          console.log("partial list to process");
-          console.log(listToProcess);
-          if(listToFill.length == 0){
-            listToProcess.map((e)=>listToFill.push("+"+e));
-          }else{
-            //For each element already present
-            listToFill.map((e,index)=>{
-              listToProcess.map((j)=>{
-                if(j != ""){
-                  console.log("Adding " + j +" to "+e+"TODO : remove"+e);
-                  listToFill.push(e+" +"+j);
-                  //listToFill.splice(index);
-                }
+        for(let listToProcess of splittedMots){
+          if(listToProcess[0] != ""){
+            console.log("partial list to process");
+            console.log(listToProcess);
+            if(listToFill.length == 0){
+              listToProcess.map((e)=>listToFill.push("+"+e));
+              console.log("Premier passage")
+              console.log(listToFill)
+            }else{
+              let tempoList = [];
+              listToFill.map(e => {
+                console.log(e+" From listToFill")
+                listToProcess.map(j=>{
+                  tempoList.push(e+" +"+j);
+                });
               });
-              listToFill.splice(listToFill.indexOf(e),1);
-            });
+              console.log("Tempo list");
+              console.log(tempoList)
+              listToFill = tempoList;
+            }
           }
         }
-        console.log("RESULTS");
-        console.log(listToFill);
-        //this.generateWordJoins(splittedMotsTemp,listToFill)
-
     }
+    return listToFill;
   }
 
-  ngOnInit(){
+  generateFormList(){
+    //Stock old words to avoid losing them
+    let tempoMots = this.mots;
+
     this.mots = [];
     for(let i=0; i<this.nbMots; i++){
       this.mots.push('');
     }
+    //Reinject old words if there's some
+    if(tempoMots){
+      tempoMots.map((e,index)=>{
+          if(index<this.nbMots){
+            this.mots[index]=e;
+          }
+        });
+    }
+
     this.indexes = Object.keys(this.mots);
+  }
+
+  ngOnInit(){
+      this.generateFormList();
   }
 }
